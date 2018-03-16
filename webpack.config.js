6652263a8baa1dee48ffe0c92caecde272dd69f1
daimebag <kevin.marilleau@gmail.com>
2018-03-16 20:41:27 +0100

    Modify webpack.config.js to load  dev and prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,8 @@
-const path = require('path');
 
+
+// CONST
 const DEBUG = process.env.NODE_ENV !== 'production';
-const IGNORE_DEFAULT = /node_modules/;
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanWebPackPlugin = require('clean-webpack-plugin'); // Todo: configure this plugin
-
-let hash = !DEBUG ? '.[contenthash]' : ''; //Todo: link hashed name file on index.html
-let css_format = `assets/css/[name]${hash}.css`;
-const extractCSS = new ExtractTextPlugin({
-    filename: css_format
-});
-const extractSASS = new ExtractTextPlugin({
-    filename: css_format
-});
-
-
-module.exports = {
-    module: {
-        rules: [{
-
-            test: /\.js$/,
-            exclude: IGNORE_DEFAULT,
-            loader: 'babel-loader'
-            },{
-
-            test: /\.(s?)css$/,
-            exclude: IGNORE_DEFAULT,
-            use: extractSASS.extract({
-                fallback: 'style-loader',
-                use: [{
-                    loader: 'css-loader',
-                    options: {
-                        minimize: true,
-                        sourceMap: DEBUG,
-                        importLoaders: 1
-                    }
-                }, {
-                    loader: 'postcss-loader',
-                    options: {
-                        sourceMap: DEBUG,
-                        plugins: (loader) => [
-                            require('autoprefixer')(),
-                            require('cssnano')()
-                        ]}
-                }, {
-                    loader: 'sass-loader'
-                }]
-            })
-        }]
-    },
-    plugins: [
-        extractSASS,
-        extractCSS
-    ],
-    devServer: {
-        compress: true,
-        contentBase: path.resolve('./dist'),
-        hot: true
-    }
-};
+module.exports = DEBUG ?
+    require('./webpack.config/webpack.config.dev') :
+    require('./webpack.config/webpack.config.prod') ;
