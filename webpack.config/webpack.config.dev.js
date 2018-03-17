e8d077b7af430b9c90fc7a42f62b9b2281732eb1
daimebag <kevin.marilleau@gmail.com>
2018-03-17 11:50:01 +0100

    - Add HTML Hot Reload function on webpack.config.dev.js

--- webpack.config/webpack.config.dev.js
+++ webpack.config/webpack.config.dev.js
@@ -3,6 +3,9 @@ const global = require('./webpack.config.global');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin'); // TODO: configure this plugin
 
+// Declare devServer for before argument on on module.devServer
+let devServer;
+
 module.exports = {
     module: {
         rules: [{
@@ -23,17 +26,40 @@ module.exports = {
             }]
         }]
     },
+    devServer: {
+        before(app, server) {
+            devServer = server;
+        },
+        compress: true,
+        contentBase: global.path.resolve('./dist'),
+        hot: true,
+        lazy: false
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './src/index.html'
         }),
+        reloadHtml,
 
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        compress: true,
-        contentBase: global.path.resolve('./dist'),
-        hot: true
-    }
+    ]
 };
+
+// Function for Hot Reload HTML
+function reloadHtml() {
+    this.plugin('compilation',
+        thing => thing.plugin('html-webpack-plugin-after-emit', trigger));
+    const cache = {};
+    function trigger(data, callback) {
+        const orig = cache[data.outputName];
+        const html = data.html.source();
+        // plugin seems to emit on any unrelated change?
+        if (orig && orig !== html)
+            devServer.sockWrite(devServer.sockets, 'content-changed');
+        cache[data.outputName] = html;
+        callback();
+    }
+}
+
+// Function to Hot Reload HTML
